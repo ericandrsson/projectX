2aa42ef1d7758b7355e5c0611bea670ca3661f67
Eric <eric.andrsson@gmail.com>
2024-09-05 12:27:24 +0300

    Automated deployment commit

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Colors for output
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Function to print colored output
+print_color() {
+    color=$1
+    message=$2
+    echo -e "${color}${message}${NC}"
+}
+
+# Export requirements
+print_color $GREEN "Exporting requirements from Poetry..."
+poetry export -f requirements.txt --output requirements.txt --without-hashes
+
 # Add all changes
 git add .
 
@@ -8,6 +25,7 @@ git commit -m "Automated deployment commit"
 git push
 
 # Build the Docker container
+print_color $GREEN "Building Docker image..."
 docker build -t dbu14txo9wibuf:latest .
 
 # Tag and push the Docker image
