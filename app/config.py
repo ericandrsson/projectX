f927f221b6abc8a435130406bbe4f0fb6c807f46
Eric <eric.andrsson@gmail.com>
2024-09-05 12:29:39 +0300

    Automated deployment commit

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = 'your-secret-key'  # Change this to a random secret key
-    POCKETBASE_URL = 'http://127.0.0.1:8090'  # Replace with your actual PocketBase URL
+    POCKETBASE_URL = os.getenv('POCKETBASE_URL', 'http://localhost:8090')
     # Add other configuration settings as needed
